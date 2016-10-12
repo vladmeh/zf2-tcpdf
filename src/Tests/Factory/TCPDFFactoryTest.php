37c9dc5bcb822f9466f303b606af11d10c7417fa
vladmeh <vladmeh@gmail.com>
2016-10-12 16:31:46 +0300

    fix tests

--- src/Tests/Factory/TCPDFFactoryTest.php
+++ src/Tests/Factory/TCPDFFactoryTest.php
@@ -8,7 +8,7 @@ class TCPDFFactoryTest extends \PHPUnit_Framework_TestCase
     private $factory;
     public function setUp()
     {
-        $this->factory = new TCPDFFactory();
+        $this->factory = new \TCPDFModule\Factory\TCPDFFactory();
     }
     public function test_it_is_initializable()
     {
