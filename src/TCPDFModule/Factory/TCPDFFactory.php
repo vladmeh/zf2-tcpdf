d4132e9742da717d896dc77874d02129c3c6ff74
vladmeh <vladmeh@gmail.com>
2016-10-13 16:39:00 +0300

    add fonts in module distr

--- src/TCPDFModule/Factory/TCPDFFactory.php
+++ src/TCPDFModule/Factory/TCPDFFactory.php
@@ -11,10 +11,10 @@ class TCPDFFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../../../data/fonts/ArialNarrow.ttf', 'TrueTypeUnicode');
-        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../../../data/fonts/ArialNarrow-Bold.ttf', 'TrueTypeUnicode');
-        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../../../data/fonts/ArialNarrow-BoldItalic.ttf', 'TrueTypeUnicode');
-        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../../../data/fonts/ArialNarrow-Italic.ttf', 'TrueTypeUnicode');
+        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../fonts/ArialNarrow.ttf', 'TrueTypeUnicode');
+        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../fonts/ArialNarrow-Bold.ttf', 'TrueTypeUnicode');
+        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../fonts/ArialNarrow-BoldItalic.ttf', 'TrueTypeUnicode');
+        TCPDF_FONTS::addTTFfont(__DIR__.'/../../../fonts/ArialNarrow-Italic.ttf', 'TrueTypeUnicode');
 
         return new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
     }
